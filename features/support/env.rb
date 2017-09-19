d1c210b0dd13e006e58477483733086a09ae2fc0
Julio González Gil <git@juliogonzalez.es>
2017-09-19 22:36:54 +0200

    Enable screenshots on failures

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,3 +9,11 @@ Capybara.run_server = false
 Capybara.default_driver = :poltergeist
 Capybara.javascript_driver = :poltergeist
 Capybara.app_host = 'http://www.google.es'
+
+# screenshots
+After do |scenario|
+  if scenario.failed?
+    encoded_img = page.driver.render_base64(:png, full: true)
+    embed("data:image/png;base64,#{encoded_img}", 'image/png')
+  end
+end
